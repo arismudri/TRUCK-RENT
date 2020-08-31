797a6cd00f075ef096f244f15d9793744e51db9f
arismudri <arismudri@gmail.com>
2020-08-31 13:32:29 +0700

    membuat migration

---
+++ database/migrations/2020_08_31_035043_create_rents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('truck_id')->constrained()->onDelete('cascade');
+            $table->date('tgl');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rents');
+    }
+}
